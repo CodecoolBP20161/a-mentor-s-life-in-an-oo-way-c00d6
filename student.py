2293fd531268af5ffda1c1f50e8e40bd25fa6785
Komlancz Janos <jkomlancz@gmail.com>
2016-06-21 14:24:10 +0200

    Student csv is updated

--- student.py
+++ student.py
@@ -1,5 +1,14 @@
 from person import Person
+import code
 
 
 class Student(Person):
-    pass
+    def __init__(self, knowledge_level, energy_level):
+        self.knowledge_level = knowledge_level
+        self.energy_level = energy_level
+
+    def use_EKI(name):
+        pass
+
+    def ask_for_help(try_count):
+        pass
