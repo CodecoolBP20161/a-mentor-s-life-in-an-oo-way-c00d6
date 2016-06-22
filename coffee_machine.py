589778ba112a8fb349eca0544af682b0c1de91e1
kalmeee <revkalman@gmail.com>
2016-06-22 10:27:22 +0200

    coffee machine 1.0

---
+++ coffee_machine.py
@@ -0,0 +1,43 @@
+class CoffeeMachine():
+    def __init__(self, is_working, coffee_level, water_level, is_clean, powered):
+        self.is_working = is_working
+        self.coffee_level = coffee_level
+        self.water_level = water_level
+        self.is_clean = is_clean
+        self.powered = powered
+
+    def toggle_power(self):
+        if self.powered:
+            self.powered = False
+        else:
+            self.powered = True
+        return(self.powered)
+
+    def get_cleaned(self):
+        if not self.is_clean:
+            self.is_clean = True
+        return(self.is_clean)
+
+    def get_filled_coffee(self):
+        self.coffee_level = 100
+        return(self.coffee_level)
+
+    def get_filled_water(self):
+        self.water_level = 100
+        return(self.water_level)
+
+    def check_if_working(self):
+        self.is_working = True
+        if not self.powered:
+            print("You gotta turn me on first bro")
+            self.is_working = False
+        elif not self.is_clean:
+            print("I'm not gona make you anything as long as i'm this filthy")
+            self.is_working = False
+        elif self.water_level == 0:
+            print("Let's trade! Gimme some water and i'll give you coffe")
+            self.is_working = False
+        elif self.coffee_level == 0:
+            print("You need coffee to make coffee bro")
+            self.is_working = False
+        return(self.is_working)
