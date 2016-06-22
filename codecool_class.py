8686fd8aafc3dc05cb6c9a6a01d187c230ae6d79
Komlancz Janos <jkomlancz@gmail.com>
2016-06-22 10:09:05 +0200

    try open_student_csv in CodecoolClass

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,13 @@ from student import Student
 
 
 class CodecoolClass:
+    
+    def open_student_csv():
+        student_list = []
+        path = os.path.abspath("./data/students.csv")
+        with open(path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                student_list.append('-'.join(row))
+        return student_list[0]
     pass
--- student.py
+++ student.py
@@ -1,14 +1,25 @@
 from person import Person
-import code
+from mentor import Mentor
+from code import Code
+import os
+import csv
 
 
 class Student(Person):
     def __init__(self, knowledge_level, energy_level):
+        Person.__init__(self, *args, **kwargs)
         self.knowledge_level = knowledge_level
         self.energy_level = energy_level
+
+
 
     def use_EKI(name):
         pass
 
-    def ask_for_help(try_count):
-        pass
+    def ask_for_help(self, mentor):
+        mentor.teach(self)
+        print("MUUUUKODIK")
+
+peti = Student
+print(peti.open_student_csv())
+peti.ask_for_help()
