ac92fc7671b9af55e8d80a4bfae683e0cf071dd3
annakertesz <kerteszannanak@gmail.com>
2016-06-24 00:47:28 +0200

    last commit before i go to bed.  management is unready.

--- management.py
+++ management.py
@@ -8,14 +8,16 @@ class Management(Person):
     def _init_(self, name):
         self.name = name
 
-    def boda_jozsi(self, mentors=CodecoolClass.mentors, students=CodecoolClass.students):
-        print('Boda Józsi has arrived!')
-        for student, mentor in students, mentors:
-            student.energy_level = 100
-            student.stress = 0
-            students.knowledge_level = 100
-            mentors.stress = 0
-            mentors.teaching_level = 100
+    def change_life(self, mentors=CodecoolClass.mentors, students=CodecoolClass.students):
+        if self.name == "Boda Józsi":
+            
+            print('Boda Józsi has arrived!')
+            for student, mentor in students, mentors:
+                student.energy_level = 100
+                student.stress = 0
+                students.knowledge_level = 100
+                mentors.stress = 0
+                mentors.teaching_level = 100
 
     def creepy_guy(self, mentors=CodecoolClass.mentors, students=CodecoolClass.students):
         for student, mentor in students, mentors:
