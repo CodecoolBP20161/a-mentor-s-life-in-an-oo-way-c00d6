ca05d93e1aa1f01e7475393f40cdb32fe39c5b2a
annakertesz <kerteszannanak@gmail.com>
2016-06-23 14:01:48 +0200

    story

--- story.py
+++ story.py
@@ -3,3 +3,12 @@ from mentor import Mentor
 from student import Student
 
 codecool_bp = CodecoolClass.generate_local()
+
+print (codecool_bp)
+print (mentor_list) #mentorok and you
+input ()
+print ("Mentors arrives at school. They welcome students with cheery smile")
+mentor_list[1].check_energy_level(student_list)
+input()
+mentor_list[0].check_mood(student_list) #stresszt beállítani kb 80ra, k_l 0-ra, hogy rossz legyen a mood
+input()
