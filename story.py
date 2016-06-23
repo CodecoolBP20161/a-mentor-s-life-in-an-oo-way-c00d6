f188a7c6b80367ff8b88427fd00a3efcf659300f
annakertesz <kerteszannanak@gmail.com>
2016-06-23 14:50:22 +0200

    mentor list

--- story.py
+++ story.py
@@ -5,9 +5,12 @@ from student import Student
 
 codecool_bp = CodecoolClass.generate_local()
 print(codecool_bp.mentors[1].full_name)
-print ('Location: ', codecool_bp.location, 'Year: ', codecool_bp.year, 'Mentors: ', codecool_bp.mentors[i].full_name for i in range(len(codecool_bp.mentors)))
+print ('Location: ', codecool_bp.location, 'Year: ', codecool_bp.year, )
+print ('Mentors: ')
+for i in range(len(codecool_bp.mentors)):
+    print ('  ', codecool_bp.mentors[i].full_name)
 #print (Mentor.create_by_csv()) #mentorok and you
-input ()
+#input("")
 print ("Mentors arrives at school. They welcome students with cheery smile")
 mentor_list[1].check_energy_level(student_list)
 input()
